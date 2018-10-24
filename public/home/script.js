0c582006fd4d75e9c9b99d831d1221815f1f4cc6
Simas2006 <sjuknelis@gmail.com>
2018-10-23 22:00:38 -0400

    Draw out all articles

---
+++ public/home/script.js
@@ -0,0 +1,43 @@
+function renderMenu(edition) {
+  var articles = edition.articles;
+  var menu = document.getElementById("menu");
+  var row = document.createElement("tr");
+  var odds = true;
+  for ( var i = 0; i < articles.length; i++ ) {
+    var col = document.createElement("td");
+    col.onclick = function() {
+      alert(this["data-id"]);
+    }
+    col["data-id"] = i;
+    var img = document.createElement("img");
+    img.src = articles[i].thumbnail;
+    col.appendChild(img);
+    var span = document.createElement("span");
+    span.innerText = articles[i].title;
+    col.appendChild(span);
+    row.appendChild(col);
+    if ( odds ) {
+      menu.appendChild(row);
+      row = document.createElement("tr");
+    }
+    odds = ! odds;
+  }
+  menu.appendChild(row);
+}
+
+function queryMenu(callback) {
+  var req = new XMLHttpRequest();
+  req.onload = function() {
+    if ( this.responseText == "Bad Request" ) {
+      alert("Failed to load the page. Please try again.");
+      return;
+    }
+    callback(JSON.parse(this.responseText));
+  }
+  req.open("GET",`/edition_data?file=${localStorage.getItem("file")}`);
+  req.send();
+}
+
+window.onload = function() {
+  queryMenu(renderMenu);
+}
