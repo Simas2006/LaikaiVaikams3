160507d84944ce0a92daf455e89fee2b590720ba
Simas2006 <sjuknelis@gmail.com>
2018-10-23 21:52:15 -0400

    Get edition data through GET command

--- server.js
+++ server.js
@@ -21,7 +21,30 @@ app.get("/article_data",function(request,response) {
     if ( data[index] ) response.send(JSON.stringify(data[index]));
     else response.send(400);
   });
-})
+});
+
+app.get("/edition_data",function(request,response) {
+  if ( request.query.file.indexOf("..") > -1 ) {
+    response.send(400);
+    return;
+  }
+  fs.readFile(`${__dirname}/editions/${request.query.file}.json`,function(err,data) {
+    if ( err ) {
+      response.send(400);
+      if ( err.code != "ENOENT" ) console.error(err);
+      return;
+    }
+    data = JSON.parse(data);
+    var format = {
+      edition: data[0].edition,
+      articles: data.map(item => {return {
+        title: item.title,
+        thumbnail: item.thumbnail
+      }})
+    }
+    response.send(JSON.stringify(format));
+  });
+});
 
 app.get("/",function(request,response) {
   response.redirect("/public");
