f51245c7ff9025dbf55e0ac8bd9639968b9639e5
Simas2006 <sjuknelis@gmail.com>
2018-10-27 13:31:48 -0400

    Draw out image object

--- public/generator/script.js
+++ public/generator/script.js
@@ -1,15 +1,22 @@
 function addImage() {
   var picker = document.getElementById("filePicker");
   picker.onchange = function() {
-    var imageObj = document.getElementById("uploaded");
     var file = this.files[0];
     var reader = new FileReader();
     reader.onloadend = function() {
-      imageObj.src = reader.result;
+      var p = document.createElement("p");
+      p.className = "image";
+      var img = document.createElement("img");
+      img.src = reader.result;
+      p.appendChild(img);
+      p.appendChild(document.createElement("br"));
+      var caption = document.createElement("input");
+      caption.className = "caption";
+      p.appendChild(caption);
+      content.appendChild(p);
     }
     if ( file ) {
       if ( file.name.toLowerCase().endsWith(".jpg") || file.name.toLowerCase().endsWith(".png") || file.name.toLowerCase().endsWith(".tiff") ) reader.readAsDataURL(file);
-      else console.log("File of invalid type");
     }
   }
   picker.click();
