e22d968f77b77e42700139faf7c4c3ffdbf1a5e5
Simas2006 <sjuknelis@gmail.com>
2018-10-27 13:07:14 -0400

    Add bottom buttons for each item

--- public/generator/script.js
+++ public/generator/script.js
@@ -1,11 +1,16 @@
-function selectFile() {
-  var imageObj = document.getElementById("uploaded");
-  var file = document.getElementById("filePicker").files[0];
-  var reader = new FileReader();
-  reader.onloadend = function() {
-    imageObj.src = reader.result;
+function addImage() {
+  var picker = document.getElementById("filePicker");
+  picker.onchange = function() {
+    var imageObj = document.getElementById("uploaded");
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      imageObj.src = reader.result;
+    }
+    if ( file ) {
+      if ( file.name.toLowerCase().endsWith(".jpg") || file.name.toLowerCase().endsWith(".png") || file.name.toLowerCase().endsWith(".tiff") ) reader.readAsDataURL(file);
+      else console.log("File of invalid type");
+    }
   }
-  if ( file ) {
-    if ( file.name.toLowerCase().endsWith(".jpg") || file.name.toLowerCase().endsWith(".png") || file.name.toLowerCase().endsWith(".tiff") ) reader.readAsDataURL(file);
-  }
+  picker.click();
 }
