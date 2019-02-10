f033d0ea74d08f9bacb9887e7e80847dd8644d25
Simas2006 <sjuknelis@gmail.com>
2019-02-10 17:23:03 -0500

    Relocate generator to generator/edit

--- compiler/dbrepl.sh
+++
@@ -1,9 +0,0 @@
-read -r -d "" SCRIPT <<- EOM
-var file;
-var load=_=>{file=JSON.parse(fs.readFileSync("$1"))};
-var save=_=>{fs.writeFileSync("$1",JSON.stringify(file));process.exit()}
-load();
-EOM
-
-echo "Loaded file \"$1\" into variable \`file\`. Use \`save()\` to save the file from memory and exit.";
-node -i -e "$SCRIPT";
