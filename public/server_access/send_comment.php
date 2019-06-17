113568f516819dd26ea3f1d93aa9338924b128c2
Simas2006 <sjuknelis@gmail.com>
2019-06-17 09:58:04 -0400

    Add server-side comment checks

--- public/server_access/send_comment.php
+++ public/server_access/send_comment.php
@@ -1,9 +1,12 @@
 <?php
 $data = file_get_contents("php://input");
-if ( is_null($data) || is_null($_GET["index"]) ) die("Bad Request");
+if ( is_null($data) || $data === "" || is_null($_GET["name"]) || $_GET["name"] === "" || is_null($_GET["index"]) ) die("Bad Request");
+if ( strlen($data) > 250 || strlen($_GET["name"]) > 50 ) die("Bad Request");
+
 $path = "../../editions/comments.json";
 $obj = @fopen($path,"r") or die("Bad Request");
 $jsonObj = json_decode(fread($obj,filesize($path)));
+
 $comment = (object) ["name" => $_GET["name"],"content" => $data];
 array_unshift($jsonObj->{$_GET["file"] . "_" . $_GET["index"]},$comment);
 fclose($obj);
