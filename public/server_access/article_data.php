c985dbfba34fd2c7e4bd38d70d6e03ead748df88
Simas2006 <sjuknelis@gmail.com>
2019-06-18 21:24:36 -0400

    Retrieve edition field from manifest file

--- public/server_access/article_data.php
+++ public/server_access/article_data.php
@@ -6,8 +6,17 @@ if ( strpos($file,"..") !== false ) die("Bad Request");
 
 $path = "../../editions/" . $file . ".json";
 $obj = @fopen($path,"r") or die("Bad Request");
-$jsonObj = json_decode(fread($obj,filesize($path)));
+$jsonObj = json_decode(fread($obj,filesize($path)),true);
 $articleObj = $jsonObj[$index];
+fclose($obj);
+
+$path = "../../editions/manifest.json";
+$obj = @fopen($path,"r") or die("Bad Request");
+$manifestObj = json_decode(fread($obj,filesize($path)),true);
+foreach ( $manifestObj as $entry ) {
+  if ( $entry["file"] == $file ) $manifestEntry = $entry;
+}
+$articleObj["edition"] = $manifestEntry["edition"];
 fclose($obj);
 echo json_encode($articleObj);
 ?>
